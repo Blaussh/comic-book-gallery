a46ab3febdb73aa261f4716ff60171b923b92ba6
Blaussh <shai.gm@gmail.com>
2017-12-22 22:25:18 +0200

    Added comic book repository

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -16,7 +16,7 @@ namespace ComicBookGallery.Controllers
             {
                 SeriesTitle = "The Amazing Spider-Man",
                 IssueNumber = 700,
-                DesctiptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
                 Artists = new Artist[]
                 {
                     new Artist() {Name = "Dan Slott", Role = "Script"},
--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -11,7 +11,7 @@ namespace ComicBookGallery.Models
         public int Id { get; set; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
-        public string DesctiptionHtml { get; set; }
+        public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
 
