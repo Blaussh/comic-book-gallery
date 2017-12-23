94a8c83a57f6c95d602abc6efe25cb8a0bc6d36a
Blaussh <shai.gm@gmail.com>
2017-12-23 17:21:57 +0200

    Added Series List view

---
+++ src/ComicBookGallery/Controllers/SeriesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ComicBookGallery.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class SeriesController : Controller
+    {
+        private SeriesRepository _seriesRepository;
+
+        public SeriesController()
+        {
+            _seriesRepository = new SeriesRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var series = _seriesRepository.GeSeries();
+            return View(series);
+        }
+    }
+}
